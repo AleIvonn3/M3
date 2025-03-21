025682237c80ce9e6d861b751cdfdca59f52a024
Ale Ivonne <aleivonne.ma@gmail.com>
2025-03-20 21:44:22 -0600

    mis valores

--- proyecto1-m3/script.js
+++ proyecto1-m3/script.js
@@ -1,4 +1,4 @@
-alert ("Holii)");
+//alert ("Holii)");
 
 console.log(typeof 42); //number
 console.log(typeof 'Veinticinco'); //string
@@ -7,4 +7,18 @@ console.log(typeof true); //boolean
 console.log(typeof 0); //number
 console.log(typeof ''); //string
 console.log(typeof null); //object
-console.log(typeof undefinedFALSE); //undefined
+console.log(typeof undefined); //undefined
+console.log(typeof FALSE); //undefined
+
+console.log(typeof jlkmad);//undefined
+console.log(typeof 35767); //number
+console.log(typeof "345#$%&#");//string
+console.log(typeof 3425);//number
+console.log(typeof !2%566);//NAN
+console.log(typeof "hola");//string
+console.log(typeof 0);  //number
+console.log(typeof -56);//number
+console.log(typeof .5616);//number
+console.log(typeof 51.654);//number
+console.log(typeof gatos);//undefined
+console.log(typeof 5.8);//number
