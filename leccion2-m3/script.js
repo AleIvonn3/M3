61a3140b42fe2b5edcccefe13bd7bf85d02341da
Ale Ivonne <aleivonne.ma@gmail.com>
2025-03-20 21:13:02 -0600

    calificaciones

--- leccion2-m3/script.js
+++ leccion2-m3/script.js
@@ -1,2 +1,14 @@
 //alert ("Hola");
 
+let nota = 90;
+
+
+if (nota >= 90) {
+    console.log("Excelente - Aprobado");
+}else if (nota >= 75) {
+    console.log("Bien - Aprobado");
+}else if (nota >= 60) {
+    console.log("Suficiente - Aprobado");
+}else if (nota < 60) {
+    console.log("Reprobado");
+}
