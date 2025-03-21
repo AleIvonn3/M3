0435074c7862565b09f608e26876db6284496335
Ale Ivonne <aleivonne.ma@gmail.com>
2025-03-20 21:22:10 -0600

    pruebas

--- leccion2-m3/script.js
+++ leccion2-m3/script.js
@@ -1,6 +1,7 @@
 //alert ("Hola");
+//45, 60, 75, 0
 
-let nota = 90;
+let nota = 65;
 
 
 if (nota >= 90) {
@@ -10,5 +11,5 @@ if (nota >= 90) {
 }else if (nota >= 60) {
     console.log("Suficiente - Aprobado");
 }else if (nota < 60) {
-    console.log("Reprobado");
+    console.log("Reprobado :( - hay que estudiar mas");
 }
