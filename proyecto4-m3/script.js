aea10b7b4f3b843e8e5f8c89174682f2c699f848
Ale Ivonne <aleivonne.ma@gmail.com>
2025-03-31 20:06:10 -0600

    funcion

---
+++ proyecto4-m3/script.js
@@ -0,0 +1,9 @@
+function librosleidos(libros){
+    console.log(libros);
+}
+
+librosleidos("Mitos y leyendas");
+librosleidos("El amor en los tiempos del colera");
+librosleidos("revolucion");
+librosleidos("poesias de amor");
+
