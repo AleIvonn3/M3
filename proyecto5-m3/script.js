e2d6e984a93178150110a0530ca865a14fdc2758
Ale Ivonne <aleivonne.ma@gmail.com>
2025-04-01 20:45:43 -0600

    funcion libro

---
+++ proyecto5-m3/script.js
@@ -0,0 +1,18 @@
+let Libro1 = {
+    titulo: "Alicia en el pais de las maravillas",
+    autor: "Lewis Carroll",
+    año: 1865,
+    genero: "Ficcion",  
+    Editorial: "Macmillan Publishers",
+    estado: "Disponible",
+
+    listadecap: { 
+        cap1: "En la madriguera del conejo",
+        cap2: "El charco de lágrimas",
+        cap3: "Una carrera electoral",
+        cap4: "El conejo envía una pequeña factura",
+    }
+    }
+
+console.log("El libro titulado " + Libro1.titulo + "es del autor " + Libro1.autor + "y fue publicado en el año " + Libro1.año + "por la editorial " + Libro1.Editorial + " este libro es de genero" + Libro1.genero + "y esta " + Libro1.estado);
+
